9e07642ebe72eaf0085dbdcc76b6a0027afbeb5f
Zade Ao <zade.ao@vrviu.com>
2020-04-21 19:25:05 +0800

    openh264 bat

--- third_party/openh264/win_x64.bat
+++ third_party/openh264/win_x64.bat
@@ -1,3 +1,5 @@
+set rootdir=%~dp0
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
 set MSYS=D:\msys64\usr\bin
@@ -11,4 +13,6 @@ set INCLUDE=%INCLUDE%
 set LIB=%lib% 
 bash -c "make OS=msvc ARCH=x86_64"
 
+xcopy %rootdir%openh264.lib  %rootdir%../lib /Y
+xcopy %rootdir%openh264.dll  %rootdir%../lib /Y
 pause
